3a2a8516464c20336a9cfd0ddc5ec79e036587b4
retrontology <retrontology@hotmail.com>
2023-08-19 04:44:41 -0500

    fix downloader

--- vgmusic/run.py
+++ vgmusic/run.py
@@ -16,7 +16,7 @@ DL_INSTANCES = 4
 
 def main():
     fetch_midi_urls()
-    #download_midis()
+    download_midis()
 
 class vgmusicDL(scrapy.Spider):
 
@@ -34,26 +34,40 @@ class vgmusicDL(scrapy.Spider):
 
     def parse_music_table(self, table):
         rows = table.xpath('tr')
-        for i in range(1, floor(len(rows)/2)):
+        for i in range(1, floor(len(rows)/2)+1):
             genre = rows[i*2-1].xpath('td/span/text()').get()
             for section in rows[i*2].xpath('td/ul/li'):
-                manufacturer = section.xpath('text()').get()
-                if manufacturer:
-                    manufacturer = manufacturer[:-1]
-                    for link in section.xpath('a'):
-                        system = link.xpath('text()').get()
-                        url = link.xpath('@href').get()
-                        print(manufacturer + " : " + system + " : " + url)
-                        callback = partial(self.parse_system, genre, manufacturer, system)
-                        yield scrapy.Request(url=url, callback=callback)
+                if genre == 'Console Systems':
+                    manufacturer = section.xpath('text()').get()
+                    if manufacturer:
+                        manufacturer = manufacturer[:-1]
+                        for link in section.xpath('a'):
+                            system = link.xpath('text()').get()
+                            url = link.xpath('@href').get()
+                            callback = partial(self.parse_system, genre, manufacturer, system)
+                            yield scrapy.Request(url=url, callback=callback)
+                elif genre == 'Computer Systems':
+                    system = section.xpath('a')
+                    url = system.xpath('@href').get()
+                    system = system.xpath('text()').get()
+                    callback = partial(self.parse_system, genre, None, system)
+                    yield scrapy.Request(url=url, callback=callback)
+                else:
+                    genre = section.xpath('a')
+                    url = genre.xpath('@href').get()
+                    genre = genre.xpath('text()').get()
+                    callback = partial(self.parse_system, genre, None, None)
+                    yield scrapy.Request(url=url, callback=callback)
 
     def parse_system(self, genre, manufacturer, system, response):
         data = {
             'genre': genre,
-            'manufacturer': manufacturer,
-            'system': system,
             'games': {}
         }
+        if system:
+            data['system'] = system
+        if manufacturer:
+            data['manufacturer'] = manufacturer
         rows = response.xpath('//table/tr')
         game = ''
         i = 0
@@ -90,20 +104,16 @@ def download_midis(midi_url_json=MIDI_URL_JSON, dl_dir=DL_DIR, dl_instances=DL_I
         midi_url_map = json.load(infile)
     for system in midi_url_map:
         for game_title in system['games']:
-            path = os.path.join(dl_dir, system['genre'], system['manufacturer'], system['system'], game_title)
+            path = os.path.join(dl_dir, system['genre'])
+            if 'manufacturer' in system:
+                path = os.path.join(path, system['manufacturer'])
+            if 'system' in system:
+                path = os.path.join(path, system['system'])
+            path = os.path.join(path, game_title)
             os.makedirs(path, exist_ok = True)
             game = system['games'][game_title]
             for midi in game:
                 downloader_pool.apply(download_file, (game[midi], midi, path))
-
-
-    for section in midi_url_map:
-        section_dir = os.path.join(dl_dir, section['name'])
-        os.makedirs(section_dir, exist_ok=True)
-        for url in section['urls']:
-            midi_path = os.path.join(section_dir, url[url.rfind('/')+1:])
-            if not os.path.isfile(midi_path):
-                downloader_pool.apply(download_file, (url, midi_path))
     downloader_pool.close()
     downloader_pool.join()
     print('All MIDIs have been downloaded!')
