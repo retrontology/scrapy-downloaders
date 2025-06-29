aba3e3765f0f19ca345cbc901cf85f7f71bc372d
retrontology <retrontology@gmail.com>
2025-06-29 06:02:42 -0500

    working atlas db scraper

--- omniatlas/run.py
+++ omniatlas/run.py
@@ -2,26 +2,26 @@ from scrapy.crawler import CrawlerProcess
 import logging
 from pathlib import Path
 from spider import AtlasSpider
+from dotenv import load_dotenv
+import os
 
 
 def main():
     logging.getLogger("urllib3").setLevel(logging.WARNING)
-    json_export = Path('output.json')
-    if json_export.exists():
-        json_export.unlink()
+    load_dotenv()
     process = CrawlerProcess(
         settings={
-            "FEEDS": {
-                json_export: {"format": "json"},
+            "ITEM_PIPELINES": {
+                "spider.AtlasFrameDBPipeline": 300,
             },
-            "MYSQL_HOST": "",
-            "MYSQL_PORT": "",
-            "MYSQL_USER": "",
-            "MYSQL_PASS": "",
-            "MYSQL_DB": "",
+            "MYSQL_HOST": os.getenv("MYSQL_HOST"),
+            "MYSQL_PORT": os.getenv("MYSQL_PORT", 3306),
+            "MYSQL_USER": os.getenv("MYSQL_USER"),
+            "MYSQL_PASS": os.getenv("MYSQL_PASS"),
+            "MYSQL_DB": os.getenv("MYSQL_DB"),
         }
     )
-    process.crawl(AtlasSpider, start_urls=["https://omniatlas.com/maps/south-america/18641213/"])
+    process.crawl(AtlasSpider)
     process.start()
     process.join()
 
--- omniatlas/spider.py
+++ omniatlas/spider.py
@@ -2,11 +2,12 @@ from scrapy import Spider
 from scrapy.crawler import Crawler
 from scrapy.item import Item, Field
 from scrapy.loader import ItemLoader
+from itemadapter.adapter import ItemAdapter
 from itemloaders.processors import TakeFirst, Join, Compose
-from datetime import datetime
-from itemadapter import ItemAdapter
 import mysql.connector
 import requests
+from urllib.parse import urlparse
+from datetime import date
 
 
 class AtlasFrameDBPipeline:
@@ -24,7 +25,7 @@ class AtlasFrameDBPipeline:
     def from_crawler(cls, crawler: Crawler):
         return cls(
             host=crawler.settings.get("MYSQL_HOST"),
-            port=crawler.settings.get("MYSQL_PORT"),
+            port=crawler.settings.get("MYSQL_PORT", 3306),
             user=crawler.settings.get("MYSQL_USER"),
             password=crawler.settings.get("MYSQL_PASS"),
             database=crawler.settings.get("MYSQL_DB"),
@@ -42,7 +43,7 @@ class AtlasFrameDBPipeline:
                     title VARCHAR(255) NOT NULL,
                     description TEXT NOT NULL,
                     url VARCHAR(255) NOT NULL,
-                    image BLOB NOT NULL
+                    image MEDIUMBLOB NOT NULL
                 )
             """
         )
@@ -64,8 +65,10 @@ class AtlasFrameDBPipeline:
 
 
     def process_item(self, item, spider):
-        cursor = self.connection.cursor()
         adapter = ItemAdapter(item)
+        frame_date = adapter.get('date')
+        frame_date = f"{frame_date.year:04d}-{frame_date.month:02d}-{frame_date.day:02d}"
+        cursor = self.connection.cursor()
         cursor.execute(
             """
                 INSERT INTO atlas_frame (
@@ -79,17 +82,17 @@ class AtlasFrameDBPipeline:
                 VALUES (%s, %s, %s, %s, %s, %s)
             """,
             (
-                adapter.get("region"),
-                adapter.get("date"),
-                adapter.get("title"),
-                adapter.get("description"),
-                adapter.get("url"),
-                adapter.get("image")
-
+                adapter.get('region'),
+                frame_date,
+                adapter.get('title'),
+                adapter.get('description'),
+                adapter.get('url'),
+                adapter.get('image')
             )
         )
         self.connection.commit()
         cursor.close()
+        return item
 
 
 class AtlasRegionFrame(Item):
@@ -102,35 +105,42 @@ class AtlasRegionFrame(Item):
 
 
 class AtlasRegionFrameLoader(ItemLoader):
+
+
+    @staticmethod
+    def parse_date(value):
+        value = value[0]
+        year, month, day = value[:-4], value[-4:-2], value[-2:]
+        return date(int(year), int(month), int(day))
+    
+
+    @staticmethod
+    def parse_image(value):
+        url = value[0].strip()
+        return requests.get(url).content
+
+
     default_output_processor = TakeFirst()
     description_in = Join('')
-    date_in = Compose(
-        lambda x: x[0],
-        lambda x: x.strip(),
-        lambda x: datetime.strptime(x, '%d %B %Y'),
-        lambda x: x.date()
-    )
-    image_in = Compose(
-        lambda x: x[0],
-        lambda x: x.strip(),
-        lambda x: requests.get(x).content
-    )
+    date_in = Compose(parse_date)
+    image_in = Compose(parse_image)
 
 
 class AtlasSpider(Spider):
     name = "omniatlas"
     start_urls = [
         "https://omniatlas.com/maps/arctic/18140114/",
-        #"https://omniatlas.com/maps/north-america/14270907/",
-        #"https://omniatlas.com/maps/south-america/18641213/",
-        #"https://omniatlas.com/maps/europe/61231/",
-        #"https://omniatlas.com/maps/northern-africa/200219/",
-        #"https://omniatlas.com/maps/sub-saharan-africa/18961026/",
-        #"https://omniatlas.com/maps/northern-eurasia/19040207/",
-        #"https://omniatlas.com/maps/east-asia/19481106/",
-        #"https://omniatlas.com/maps/southern-asia/19020115/",
-        #"https://omniatlas.com/maps/asia-pacific/18940609/",
-        #"https://omniatlas.com/maps/australasia/17880126/"
+        "https://omniatlas.com/maps/north-america/14270907/",
+        "https://omniatlas.com/maps/south-america/18641213/",
+        "https://omniatlas.com/maps/europe/61231/",
+        "https://omniatlas.com/maps/northern-africa/200219/",
+        "https://omniatlas.com/maps/sub-saharan-africa/18961026/",
+        "https://omniatlas.com/maps/northern-eurasia/19040207/",
+        "https://omniatlas.com/maps/east-asia/19481106/",
+        "https://omniatlas.com/maps/southern-asia/19020115/",
+        "https://omniatlas.com/maps/asia-pacific/18940609/",
+        "https://omniatlas.com/maps/australasia/17880126/",
+        "https://omniatlas.com/maps/eastern-mediterranean/61123/"
     ]
 
 
@@ -138,7 +148,8 @@ class AtlasSpider(Spider):
     def parse_frame(response) -> AtlasRegionFrame:
         loader = AtlasRegionFrameLoader(item=AtlasRegionFrame(), response=response)
         loader.add_xpath("region", '/html/body/main/div[1]/div/div[2]/div/section[1]/div[1]/div[3]/h3/a/text()')
-        loader.add_xpath("date", '/html/body/main/div[1]/div/div[2]/div/section[1]/div[1]/div[1]/h3/a/text()')
+        loader.add_value("date", urlparse(response.url).path.rsplit('/', 2)[1])
+        #loader.add_xpath("date", '/html/body/main/div[1]/div/div[2]/div/section[1]/div[1]/div[1]/h3/a/text()')
         loader.add_xpath("title", '/html/body/main/div[1]/div/div[2]/div/section[2]/h2/a/text()')
         loader.add_xpath("image", '/html/body/main/div[1]/div/div[2]/div/section[2]/div[2]/div/div[2]/a[1]/@href')
         loader.add_xpath("description", '/html/body/main/div[1]/div/div[2]/div/section[2]/div[2]/p/text() | /html/body/main/div[1]/div/div[2]/div/section[2]/div[2]/p/a/text()')
