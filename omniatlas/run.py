e4bd2e04b7ad683cafd992c7a569b6a027220553
retrontology <retrontology@gmail.com>
2025-07-06 09:08:54 -0500

    fix pipeline name

--- omniatlas/run.py
+++ omniatlas/run.py
@@ -12,14 +12,14 @@ def main():
     load_dotenv()
     settings = {
         "ITEM_PIPELINES": {
-            "spider.AtlasFrameMySQLPipeline": 300,
+            "spider.AtlasFramePipeline": 300,
         },
         "MYSQL_HOST": os.getenv("MYSQL_HOST"),
         "MYSQL_PORT": os.getenv("MYSQL_PORT", 3306),
         "MYSQL_USER": os.getenv("MYSQL_USER"),
         "MYSQL_PASS": os.getenv("MYSQL_PASS"),
         "MYSQL_DB": os.getenv("MYSQL_DB"),
-        "IMAGE_DIR": os.getenv("IMAGE_DIR", 'images')
+        "IMAGE_DIR": os.getenv("IMAGE_DIR", 'imagesmysqlpip')
     }
     process = CrawlerProcess(
         settings=settings
